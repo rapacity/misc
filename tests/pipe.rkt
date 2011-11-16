ec44ddbc6617ec83462625863a1a57828d4119b9
Khalil Harake <rapacity@codealife.com>
2011-11-16 22:35:33 +0200

    reversed test arguments

--- tests/pipe.rkt
+++ tests/pipe.rkt
@@ -2,20 +2,23 @@
 
 (require rackunit "../pipe.rkt")
 
-(test-equal? "1 expr 1 value" 3333 (->> 3333))
+(test-equal? "1 expr 1 value" (->> 3333) 3333)
 
-(test-equal? "2 expr 2 value pipe" "{<<1234>>}" (->> (values "<<~a>>" 1234)
-                                                     (format (string-append "{" <> "}") <>)))
+(test-equal? "2 expr 2 value pipe" (->> (values "<<~a>>" 1234)
+                                        (format (string-append "{" <> "}") <>))
+             "{<<1234>>}")
 
-(test-equal? "2 expr 1 value pipe" "{1234}" (->> "1234" (format "{~a}" <>)))
+(test-equal? "2 expr 1 value pipe" (->> "1234" (format "{~a}" <>)) "{1234}")
 
 (test-equal? "multiple expr multiple value pipe"
-             "<8>"
              (->> (values 1 2 3 4)
                   (values <> (+ <> <> <>))
                   (let ([a <>] [b <>])
                     (- b a))
-                  (format "<~a>" <>)))
+                  (format "<~a>" <>))
+             "<8>")
+
+(test-equal? "non-sexp final arg" (->> 1 <>) 1)
 
 (define x
   (->>* (values <> (+ <> <> <>))
@@ -23,6 +26,6 @@
           (- b a))
         (format "<~a>" <>)))
 
-(test-equal? "function multiple expr multiple value pipe" "<8>" (x 1 2 3 4))
+(test-equal? "function multiple expr multiple value pipe" (x 1 2 3 4) "<8>")
 
-(test-equal? "function no args" "<8>" ((->>* "<8>")))
+(test-equal? "function no args" ((->>* "<8>")) "<8>")
